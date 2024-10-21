03862546d9c831befef8c1c5d8d546d2b3bf6204
Jarvis-Li2244 <144480728+Jarvis-Li2244@users.noreply.github.com>
2024-10-21 13:57:42 -0400

    zxXXXZXWS DfzgBFFGDRTDHCdr

---
+++ app.js
@@ -0,0 +1,24 @@
+function factorial(x) {
+    if (n < 1) {
+        return;
+    }
+    let result = 1;
+    for (let i = 1; i <= x; i++) {
+        result *= i
+    };
+    return result;
+}
+
+console.log(factorial(7));
+
+function newFactorial(y) {
+    if(n > 0) {
+        let result = 1;
+        for (let i = 1; i <= x; i++) {
+            result *= i
+        };
+        return result;
+    } else {
+        return;
+    }
+}
