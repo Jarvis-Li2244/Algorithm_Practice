eb26275be74afa533edf5583498140d6c2484442
Jarvis-Li2244 <144480728+Jarvis-Li2244@users.noreply.github.com>
2024-10-24 13:58:36 -0400

    asdad

--- app.js
+++ app.js
@@ -34,3 +34,42 @@ function occupiedSpaces(n,x,y) {
 }
 
 console.log(occupiedSpaces(5, "CCCCC", "C.C.."))
+
+// Psuedocode for Decimal to Binary
+function binary(a) {
+    let number = ""
+    let y = 2
+    while (y <= a) {
+        y *= 2
+    }
+    y /= 2
+    while (y > 1) {
+        if (y <= a) {
+            a = a % y
+            number += "1"
+        } else {
+            number += "0"
+        }
+        y /= 2
+    }
+    if (a == 1) {
+        number += "1"
+    } else {
+        number += "0"
+    }
+    let x = Number(number)
+    return x
+}
+
+function decimal(a) {
+    let x = String(a)
+    let number = 0
+    for (let i = 0; i < x.length; i++) {
+        if (x[i] == 1) {
+            number += 2**(x.length - 1 - i)
+        }
+    }
+    return number
+}
+
+
